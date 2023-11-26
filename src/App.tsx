0e5ce0a0aad5f46c696e4eed1e5642be6f0adf78
Dani Arthur Widodo <work.daniarthurwidodo@gmail.com>
2023-11-27 06:23:38 +0700

    format + add basic form bahaviour with object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [form1, setForm1] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const submitForm = (event: any) => {
+    event.preventDefault();
+    console.log(form1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="name">name : </label>
+
+      <form onSubmit={submitForm}>
+        <div >
+          <input
+            type="text"
+            id="name"
+            value={form1.name}
+            onChange={(e) => {
+              setForm1({ ...form1, name: e.target.value });
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">email : </label>
+
+          <input
+            type="email"
+            id="email"
+            value={form1.email}
+            onChange={(e) => {
+              setForm1({ ...form1, email: e.target.value });
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">password : </label>
+
+          <input
+            type="password"
+            id="password"
+            value={form1.password}
+            onChange={(e) => {
+              setForm1({ ...form1, password: e.target.value });
+            }}
+          />
+        </div>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
